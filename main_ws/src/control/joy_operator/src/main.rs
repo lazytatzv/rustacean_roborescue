f589b82634b60245ca6e32630733eba96183349b
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-02-03 18:58:35 +0900

    Write test joy_operator

--- main_ws/src/control/joy_operator/src/main.rs
+++ main_ws/src/control/joy_operator/src/main.rs
@@ -1,5 +1,20 @@
 use rclrs::*;
+use sensor_msgs;
+use anyhow::Result;
 
-let context Context::default_from_env()?
-let mut executor = context.create_basic_executor();
-let node = executor.create_node("joy_controller")?;
+fn main() -> Result<()> {
+    let context = Context::default_from_env()?;
+    let mut executor = context.create_basic_executor();
+    let node = executor.create_node("joy_operator")?;
+
+    let subscription = node.create_subscription(
+        "/joy",
+        |msg: sensor_msgs::msg::Joy| {
+            println!("Pressed: {}", msg.buttons[0]);
+        }
+    )?;
+
+    executor.spin(SpinOptions::default()).first_error()?;
+
+    Ok(())
+}
