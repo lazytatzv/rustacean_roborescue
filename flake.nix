9b0c79f275fb6bc62deedf6a9c67ed4b9745f966
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-03-22 23:50:41 +0900

    nix: make pythonEnv minimal (numpy, opencv4, pytest) to satisfy CMake/NumPy

--- flake.nix
+++ flake.nix
@@ -227,7 +227,7 @@
         };
 
         # Explicit Python environment with common developer packages (NumPy etc.)
-        pythonEnv = pkgs.python3.withPackages (ps: with ps; [ numpy opencv4 black isort (ps."pre-commit") cpplint (ps."pip-audit") pytest ]);
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [ numpy opencv4 pytest ]);
 
       in
       {
